4a374bb8718d027bae35ed37e3dc5fa5a716ca72
mjezewsk <marcin.jezewski@nokia.com>
2019-08-29 17:12:22 +0200

    basic deciding made

--- first_idea.py
+++ first_idea.py
@@ -7,33 +7,15 @@ from itertools import zip_longest
 
 def count_chars(row):
     result = {}
-    # TODO dla kazdego znaku w kolumnie:
-    # okreslic typ by nie liczyc niepotrzebnych prawdopodobienstw
-    # sprawdzic czy byl on juz brany do liczenia  : jezeli tak to dodac 1 do danego elementu jezeli nie to dodac element z wartoscia 1
     for element in row:
         if element not in result.keys():
             result.update({element: row.count(element)})
     result.update({"_all": len(row)})
-    # for char in result:
-    #     result[char]=result[char]/all_elements
-    # for digit in string.digits:
-    #     result.update(
-    #         {"{}".format(digit): row.count("{}".format(digit)) / all_elements}
-    #     )
-    # for letter in string.ascii_letters:
-    #     result.update(
-    #         {"{}".format(letter): row.count("{}".format(letter)) / all_elements}
-    #     )
-    # for extra_char in string.punctuation:
-    #     result.update(
-    #         {"{}".format(extra_char): row.count("{}".format(extra_char)) / all_elements}
-    #     )
     return result
 
 
 def calculate_propablities(row):
     output = {}
-    print(row.keys())
     for element in row.keys():
         if element is not "_all":
             output.update({element: row[element] / row["_all"]})
@@ -53,6 +35,17 @@ def calculate_propablities(row):
                 else:
                     output["/W"] = row[element] / row["_all"]
     return output
+
+def decide_of_char(place_candidats):
+    candidates = []
+    for candidate in place_candidats.keys():
+        if candidate is not ("/d" or "/w" or "/W") and place_candidats[candidate] >= 0.99:
+            return candidate
+        if place_candidats[candidate] >= 0.9:
+            return candidate
+
+    return "/."
+
 
 
 def find_lengths_spotted_in(data):
@@ -82,12 +75,11 @@ def split_data_in_clean_and_garbage(dataset, most_common_length):
 
 def transpose_string_matrix(list_of_strings):
     chars = []
-    for row in cleaned:
+    for row in list_of_strings:
         chars.append(list(row))
     return list(zip_longest(*chars))
 
-
-if __name__ == "__main__":
+def main():
     with open("datasets/de_ibans.datasets", "r") as opened_file:
         datas = opened_file.read()
     cleaned, garbage = split_data_in_clean_and_garbage(
@@ -100,3 +92,8 @@ if __name__ == "__main__":
         calculate_propablities(count_chars(row)) for row in main_array
     ]
     pprint.pprint(list_of_propably_types)
+    pprint.pprint([decide_of_char(place)for place in list_of_propably_types])
+
+
+if __name__ == "__main__":
+    main()
